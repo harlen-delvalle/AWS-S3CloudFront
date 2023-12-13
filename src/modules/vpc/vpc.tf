bcb4bfe4f819d0f8f55f21ff987f108278d5eebc
andres0010 <jonnyandres0010@hotmail.com>
2023-12-13 16:15:09 -0500

    Update vpc.tf

--- src/modules/vpc/vpc.tf
+++ src/modules/vpc/vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "ccVPC" {
   cidr_block       = var.vpc_cidr
-  enable_dns_hostnames = "enable"
+  enable_dns_hostnames = true
   instance_tenancy = "default"
   tags = {
     Name    = "ccVPC"
