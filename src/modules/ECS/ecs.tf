37c8b9a993a54acb624da3c6514fd6e6cb02bff0
andres0010 <jonnyandres0010@hotmail.com>
2023-12-13 17:42:37 -0500

    Update ecs.tf

--- src/modules/ECS/ecs.tf
+++ src/modules/ECS/ecs.tf
@@ -1,1 +1,114 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.subnet_cidr_blocks[0]
+  availability_zone       = "us-east-1a" # Cambia según tu región
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.subnet_cidr_blocks[1]
+  availability_zone       = "us-east-1b" # Cambia según tu región
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "ecs_security_group" {
+  name        = "ecs_security_group"
+  description = "Allow traffic for ECS service"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/8"] # Ajusta según tus necesidades
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "my_lb" {
+  name               = "my-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ecs_security_group.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = var.container_port
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = var.task_family
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  cpu    = "256"
+  memory = "512"
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name  = "my-container",
+    image = var.container_image,
+    portMappings = [
+      {
+        containerPort = var.container_port,
+        hostPort      = var.container_port,
+      },
+    ],
+  }])
+}
+
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    security_groups = [aws_security_group.ecs_security_group.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+    container_name   = "my-container"
+    container_port   = var.container_port
+  }
+}
