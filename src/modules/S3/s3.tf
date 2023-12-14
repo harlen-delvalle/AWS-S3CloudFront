692d2493effd80af307cfbd001c54b46169db434
harlen.delvalle <harlen.delvalle@gmail.com>
2023-12-14 18:57:25 -0500

    Update s3.tf

--- src/modules/S3/s3.tf
+++ src/modules/S3/s3.tf
@@ -17,3 +17,22 @@ resource "aws_s3_bucket" "my_bucket" {
     }]
   })
 }
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.my_bucket.bucket_regional_domain_name
+    origin_id   = "S3Origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = var.index_document
+
+  # Resto de la configuración de CloudFront...
+}
+
+output "cloudfront_domain_name" {
+  description = "Nombre de dominio de CloudFront"
+  value       = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
