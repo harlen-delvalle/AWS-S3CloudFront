b994ade1e753c32bb4e74d32dd963f9d1e1cad48
harlen.delvalle <harlen.delvalle@gmail.com>
2023-12-14 16:05:35 -0500

    Update variables.tf

--- src/modules/ECS/variables.tf
+++ src/modules/ECS/variables.tf
@@ -31,7 +31,7 @@ variable "task_family" {
 variable "container_image" {
   description = "Imagen del contenedor para la tarea ECS."
   type        = string
-  default     = "public.ecr.aws/nginx/nginx:latest"
+  default     = "nginx:stable-alpine3.17"
 }
 
 variable "container_port" {
