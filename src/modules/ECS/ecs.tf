fcf75f6884040e16c6ecf081191682ad47ddd03c
andres0010 <jonnyandres0010@hotmail.com>
2023-12-13 19:18:15 -0500

    Update ecs.tf

--- src/modules/ECS/ecs.tf
+++ src/modules/ECS/ecs.tf
@@ -53,6 +53,7 @@ resource "aws_lb_target_group" "my_target_group" {
   port     = var.container_port
   protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc.id
+  target_type = "ip"  # Ajuste importante para Fargate con awsvpc
 
   health_check {
     path     = "/health"  # Ajusta según las necesidades de tu aplicación
