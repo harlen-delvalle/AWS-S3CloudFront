52f554a16f8ebde44a848687eedb5df8014cb157
andres0010 <jonnyandres0010@hotmail.com>
2023-12-13 20:35:35 -0500

    Update ecs.tf

--- src/modules/ECS/ecs.tf
+++ src/modules/ECS/ecs.tf
@@ -40,9 +40,15 @@ resource "aws_security_group" "ecs_security_group" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "my_lb_attachment" {
-  target_group_arn = aws_lb_target_group.my_target_group.arn
-  target_id        = aws_ecs_service.my_service.id
+resource "aws_lb_listener" "my_lb_listener" {
+  load_balancer_arn = aws_lb.my_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
 }
 
 resource "aws_lb" "my_lb" {
