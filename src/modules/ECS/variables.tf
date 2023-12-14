26633572bbe85444324a7fa9a8ce3bde82f48cf9
andres0010 <jonnyandres0010@hotmail.com>
2023-12-13 21:58:07 -0500

    Update variables.tf

--- src/modules/ECS/variables.tf
+++ src/modules/ECS/variables.tf
@@ -1,145 +1,41 @@
-provider "aws" {
-  region = var.region
+variable "region" {
+  description = "La región de AWS donde se desplegarán los recursos."
+  type        = string
+  default     = "us-east-1" # Cambia según tu región preferida
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr_block
+variable "vpc_cidr_block" {
+  description = "Bloque CIDR para la VPC."
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = var.subnet_cidr_blocks[0]
-  availability_zone       = "us-east-1a" # Cambia según tu región
-  map_public_ip_on_launch = true
+variable "subnet_cidr_blocks" {
+  description = "Bloques CIDR para las subredes públicas."
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = var.subnet_cidr_blocks[1]
-  availability_zone       = "us-east-1b" # Cambia según tu región
-  map_public_ip_on_launch = true
+variable "ecs_cluster_name" {
+  description = "Nombre del clúster ECS."
+  type        = string
+  default     = "my-cluster"
 }
 
-resource "aws_security_group" "ecs_security_group" {
-  name        = "ecs_security_group"
-  description = "Allow traffic for ECS service"
-  vpc_id      = aws_vpc.my_vpc.id # Asociar la VPC al grupo de seguridad
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/8"] # Ajusta según tus necesidades
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+variable "task_family" {
+  description = "Familia de la tarea ECS."
+  type        = string
+  default     = "my-app"
 }
 
-resource "aws_lb_listener" "my_lb_listener" {
-  load_balancer_arn = aws_lb.my_lb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my_target_group.arn
-  }
+variable "container_image" {
+  description = "Imagen del contenedor para la tarea ECS."
+  type        = string
+  default     = "nginx:latest"
 }
 
-resource "aws_lb" "my_lb" {
-  name               = "my-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.ecs_security_group.id]
-  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-}
-
-resource "aws_lb_target_group" "my_target_group" {
-  name     = "my-target-group"
-  port     = var.container_port
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.my_vpc.id
-  target_type = "ip"  # Ajuste importante para Fargate con awsvpc
-
-  health_check {
-    path     = "/health"  # Ajusta según las necesidades de tu aplicación
-    port     = var.container_port
-    protocol = "HTTP"
-  }
-}
-
-resource "aws_ecs_cluster" "my_cluster" {
-  name = var.ecs_cluster_name
-}
-
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecs_execution_role"
-  
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_ecs_task_definition" "my_task" {
-  family                   = var.task_family
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-
-  cpu    = "512"
-  memory = "1024"
-
-  execution_role_arn = aws_iam_role.ecs_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name  = "my-container",
-    image = var.container_image,
-    portMappings = [
-      {
-        "containerPort" = var.container_port,
-        "hostPort"      = var.container_port,
-      },
-    ],
-    healthCheck = {
-      command     = ["CMD-SHELL", "curl -f http://localhost/health || exit 1"]
-      interval    = 30
-      timeout     = 5
-      startPeriod = 60
-      retries     = 3
-    }
-  }])
-}
-
-resource "aws_ecs_service" "my_service" {
-  name            = "my-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.my_task.arn
-  desired_count = 2
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-    security_groups = [aws_security_group.ecs_security_group.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.my_target_group.arn
-    container_name   = "my-container"
-    container_port   = var.container_port
-  }
-}
-
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+variable "container_port" {
+  description = "Puerto del contenedor ECS."
+  type        = number
+  default     = 80
 }
