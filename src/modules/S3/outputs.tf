a608f2df87fdd91200596fba9661ff98512cb662
harlen.delvalle <harlen.delvalle@gmail.com>
2023-12-14 19:29:46 -0500

    Update outputs.tf

--- src/modules/S3/outputs.tf
+++ src/modules/S3/outputs.tf
@@ -2,3 +2,8 @@ output "cloudfront_domain_name" {
   description = "Nombre de dominio de CloudFront"
   value       = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output "other_output" {
+  description = "Otras salidas"
+  value       = "Valor"
+}
