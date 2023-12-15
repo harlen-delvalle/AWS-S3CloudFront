03581507b3b34cbbad0aac5b5f5b66da5eb9114b
harlen.delvalle <harlen.delvalle@gmail.com>
2023-12-14 21:00:32 -0500

    Delete src/modules/S3/outputs.tf

--- src/modules/S3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cloudfront_domain_name" {
-  description = "Nombre de dominio de CloudFront"
-  value       = aws_cloudfront_distribution.s3_distribution.domain_name
-}
-
-output "other_output" {
-  description = "Otras salidas"
-  value       = "Valor"
-}
