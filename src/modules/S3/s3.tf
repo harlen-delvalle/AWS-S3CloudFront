200d8621d2c0e06ba4231a342b2623b79b1e1401
harlen.delvalle <harlen.delvalle@gmail.com>
2023-12-14 20:17:32 -0500

    Update s3.tf

--- src/modules/S3/s3.tf
+++ src/modules/S3/s3.tf
@@ -1,38 +1,75 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = "*",
-      Action    = "s3:GetObject",
-      Resource  = "${aws_s3_bucket.my_bucket.arn}/*",
-      Condition = {
-        StringLike = {
-          "aws:Referer": "http://dxxxxxxxxxx.cloudfront.net/*"
-        }
-      }
-    }]
-  })
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-bucket-from-terraform"
+  #acl    = "private"
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "My OAI"
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "confundus-policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+resource "aws_cloudfront_distribution" "distribution" {
   origin {
-    domain_name = aws_s3_bucket.my_bucket.bucket_regional_domain_name
-    origin_id   = "S3Origin"
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.bucket.id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = var.index_document
+  comment             = "My distribution"
+  default_root_object = "index.html"
 
-  # Resto de la configuración de CloudFront...
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.bucket.id
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
-
-output "cloudfront_domain_name" {
-  description = "Nombre de dominio de CloudFront"
-  value       = aws_cloudfront_distribution.s3_distribution.domain_name
-}
-
